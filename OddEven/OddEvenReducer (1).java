262f1714192c2cbeb7838faa2822a32ff4a94c50
kaviyamanoharan <102371722+kaviyamanoharan@users.noreply.github.com>
2023-03-06 22:25:53 +0530

    Add files via upload

---
+++ OddEven/OddEvenReducer (1).java
@@ -0,0 +1,25 @@
+package com.oddeven;
+
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class OddEvenReducer extends MapReduceBase implements Reducer<Text,IntWritable,Text,String>{
+	public void reduce(Text key,Iterator<IntWritable> values,OutputCollector<Text,String> output,Reporter reporter)
+	throws IOException{
+		String s =" ";
+		while(values.hasNext()) //check if scanner as another token in its input
+		{
+			IntWritable temp=values.next();
+			s += temp + " ";
+		}
+		output.collect(key, new String(s));
+		}
+}
+
+
