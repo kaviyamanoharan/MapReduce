10bb17640f975114cdcdb5dc187cc5934fc85286
kaviyamanoharan <102371722+kaviyamanoharan@users.noreply.github.com>
2023-03-09 22:51:59 +0530

    Add files via upload

---
+++ Map Only Mapper/OEmap1.java
@@ -0,0 +1,31 @@
+package com.oddevenmap;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+
+public class OEmap1 extends MapReduceBase implements Mapper<LongWritable,Text,Text,IntWritable>{
+	public void map(LongWritable key,Text value,OutputCollector<Text,IntWritable> output,Reporter reporter)
+	throws IOException{
+		String data[]=value.toString().split(" ");
+		for(String num :data){
+			int number =Integer.parseInt(num);
+			if (number %2 ==0)
+			{
+				output.collect(new Text("Even"),new IntWritable(number));
+			}
+			else{
+				output.collect(new Text("Odd"),new IntWritable(number));
+			}
+		}
+	}
+}
+
+
